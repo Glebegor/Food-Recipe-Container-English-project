15203890c610975734bf5ce56740b67d93d1063b
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-07-13 00:30:30 +0200

    Working with backend.

---
+++ backend/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+type Config struct {
+    DBName string
+    DBUser string
+    DBPassword string
+    DBHost string
+    DBPort string
+    SERVERPort string
+    SERVERHost string
+}
+
+
+func main() {
+    logrus.SetFormatter(&logrus.JSONFormatter{})
+
+    // Config
+    conf := Config{
+        DBName: "food-test",
+        DBUser: "postgres",
+        DBPassword: "123321",
+        DBHost: "localhost",
+        DBPort: "5436",
+        SERVERHost: "localhost",
+        SERVERPort: "8080",
+    }
+
+    return;
+}
